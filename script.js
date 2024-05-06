a94a014ec13ef572f2f19adc54216cf5a3d2e212
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-06 19:15:19 +0300

    complete color changing task

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
+
 for (let j = 1; j < 17; j++){
     lineContainer = document.createElement("div");
     lineContainer.classList.add("linecontainer");
 
     for (let i = 1; i < 17; i++) {
         let newDiv = document.createElement("div");
+        newDiv.classList.add("innerdiv");
         newDiv.style.flex = '1 0 auto';
         newDiv.style.border = '1px solid black';
         newDiv.style.aspectRatio = '1 / 1';
@@ -13,3 +15,11 @@ for (let j = 1; j < 17; j++){
 
     container.appendChild(lineContainer);
 }
+
+const innerDivs = document.querySelectorAll(".innerdiv");
+
+innerDivs.forEach( (element) => {
+    element.addEventListener("mouseenter", () => {
+        element.style.backgroundColor = "orchid";
+    });
+});
